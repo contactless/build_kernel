89ecff3cc9b3ae6b6105627e4ac1e8bed1513d14
Robert Nelson <robertcnelson@gmail.com>
2013-01-23 11:36:42 -0600

    3.7.4: and script changes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ imx_bootlets_target="imx23-olinuxino"
 
 #Kernel/Build
 KERNEL_REL=3.7
-KERNEL_TAG=${KERNEL_REL}.2
-BUILD=imxv5-x0.4
+KERNEL_TAG=${KERNEL_REL}.4
+BUILD=imxv5-x0.5
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="fa4c95bfdb85d568ae327d57aa33a4f55bab79c4"
