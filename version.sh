ba3e3c93e2760e78b5ecaa325c1586b6759132d9
Robert Nelson <robertcnelson@gmail.com>
2013-03-04 09:55:01 -0600

    bump: v3.8.2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -23,8 +23,8 @@ config="at91_dt_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.8
-KERNEL_TAG=${KERNEL_REL}.1
-BUILD=armv5-x0.15
+KERNEL_TAG=${KERNEL_REL}.2
+BUILD=armv5-x0.16
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
