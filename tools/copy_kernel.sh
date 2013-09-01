699d5d44362e8acb379ad23ee5c344179bcc4b45
Evgeny Boger <eugenyboger@gmail.com>
2013-09-02 00:39:06 +0400

    Add copy kernel script

---
+++ tools/copy_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+test $# -ge 1  || echo "USAGE: tools/copy_kernel.sh <path to rootfs>" && exit 1
+
+
+
+sudo tar xfv deploy/3.11.0-rc7-imxv5-x0.7-firmware.tar.gz -C $1/lib/firmware/
+sudo tar xfv deploy/3.11.0-rc7-imxv5-x0.7-modules.tar.gz -C $1/
+sudo tar xfv deploy/3.11.0-rc7-imxv5-x0.7-dtbs.tar.gz -C $1/boot/dtbs/
+sudo cp deploy/3.11.0-rc7-imxv5-x0.7.zImage  $1/boot/zImage
