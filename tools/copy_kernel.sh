9ecb6d9e0d2ee89ab99dd1d0c1d8b457ffe10942
Evgeny Boger <eugenyboger@gmail.com>
2013-10-06 20:46:42 +0400

    get location from env

--- tools/copy_kernel.sh
+++ tools/copy_kernel.sh
@@ -6,8 +6,9 @@ then
   exit 1
 fi
 
+echo "kernl version is $kernel_version"
 
-sudo tar xfv deploy/3.11.0-rc7-imxv5-x0.7-firmware.tar.gz -C $1/lib/firmware/
-sudo tar xfv deploy/3.11.0-rc7-imxv5-x0.7-modules.tar.gz -C $1/
-sudo tar xfv deploy/3.11.0-rc7-imxv5-x0.7-dtbs.tar.gz -C $1/boot/dtbs/
-sudo cp deploy/3.11.0-rc7-imxv5-x0.7.zImage  $1/boot/zImage
+sudo tar xfv deploy/$kernel_version-firmware.tar.gz -C $1/lib/firmware/
+sudo tar xfv deploy/$kernel_version-modules.tar.gz -C $1/
+sudo tar xfv deploy/$kernel_version-dtbs.tar.gz -C $1/boot/dtbs/
+sudo cp deploy/$kernel_version.zImage  $1/boot/zImage
