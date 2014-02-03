82ff7d8f4845e5e627b531e374a5e49fbbd64c41
Evgeny Boger <eugenyboger@gmail.com>
2014-02-03 07:31:23 +0400

    up to 3.13

--- version.sh
+++ version.sh
@@ -25,15 +25,15 @@ linaro_toolchain="arm9_gcc_4_7"
 #linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
-KERNEL_REL=3.12
-KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=imxv5-x0.3
+KERNEL_REL=3.13
+KERNEL_TAG=${KERNEL_REL}
+BUILD=imxv5-x0.1
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="v3.12.x-imxv5"
+BRANCH="v3.13.x-imxv5"
 
 BUILDREV=1.0
 DISTRO=cross
