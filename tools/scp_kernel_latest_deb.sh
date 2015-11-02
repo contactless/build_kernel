5e3acae5adcdee14e073f03e3faf93663e00e073
Evgeny Boger <boger@contactless.ru>
2015-11-03 01:50:57 +0300

    allows passing port to scp_kernel

--- tools/scp_kernel_latest_deb.sh
+++ tools/scp_kernel_latest_deb.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -gt 2 ] || [ $# -lt 1 ]
 then
-  echo "USAGE: tools/scp_kernel.sh root@host"
+  echo "USAGE: tools/scp_kernel.sh root@host [port]"
   exit 1
 fi
 
+PORT="${2:-22}"
+echo $PORT
 
 
-scp deploy/linux-image_armel.deb deploy/linux-firmware-image_armel.deb $1:
-ssh $1 "dpkg -i linux-image_armel.deb linux-firmware-image_armel.deb"
+scp -P ${PORT} deploy/linux-image_armel.deb deploy/linux-firmware-image_armel.deb $1:
+ssh -p $PORT $1 "dpkg -i linux-image_armel.deb linux-firmware-image_armel.deb"
 
 
