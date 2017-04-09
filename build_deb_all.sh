fb6559c5c652fc74a7bc4860f274985322c94064
Alexey Ignatov <lexszero@gmail.com>
2017-04-09 18:39:35 +0300

    Add build_deb_all.sh to build full set of packages sharing the same version

---
+++ build_deb_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+export BUILDREV=`date -u +%Y%m%d%H%M%S`
+. ./version.sh
+
+for KERNEL_FLAVOUR in wb2 wb6; do
+	# get DEBARCH for current flavour
+	setup_kernel_vars
+
+	export WBDEV_TARGET=wheezy-${DEBARCH}
+	wbdev user KERNEL_FLAVOUR=${KERNEL_FLAVOUR} BUILDREV=${BUILDREV} ./build_deb.sh
+done
+
+for DEBARCH in armel armhf; do
+	export WBDEV_TARGET=wheezy-${DEBARCH}
+	wbdev user DEBARCH=${DEBARCH} BUILDREV=${BUILDREV} ./build_deb_dtc.sh
+done
